ab095fc8415ced97a41adb79793fbd3ca971e422
Sathya <panchasathya007@gmail.com>
2024-04-28 16:18:20 +0700

    codes

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ back_color = (200, 255, 255)
 window = display.set_mode((800, 500))
 display.set_caption("Pong Time")
 
+font.init()
+
 class GameSprite(sprite.Sprite):
     def __init__(self, img, x, y, w, h, speed):
         super().__init__()
@@ -30,26 +32,58 @@ class Player(GameSprite):
 
 clock = time.Clock()
 
-player_l = Player("images/paddle.png", 10, 200, 120 , 150 ,5)
-player_r = Player("images/paddle.png", 700, 200, 120 , 150 ,5)
-ball = GameSprite("images/ball.png", 400, 200, 35, 35, 5)
+player_l = Player("images/paddle.png", 10, 200, 70 , 180 ,5)
+player_r = Player("images/paddle.png", 700, 200, 70 , 180 ,5)
+ball = GameSprite("images/ball.png", 400, 200, 35, 35, 3)
+
+font_game = font.Font(None, 30)
+player_l_lose = font_game.render(
+    "Player 1 Loses!", 1, (255, 0, 0)
+    )
+
+player_r_lose = font_game.render(
+    "Player 2 Loses!", 1, (255, 0, 0)
+    )
+
+ball_speed_x = ball.speed
+ball_speed_y = ball.speed
 
 def reset(player: Player):
     window.blit(player.image, (player.rect.x, player.rect.y))
 
 running = True
+finish = False
 while running:
-    for ev in event.get():
-        if ev.type == QUIT:
-            running = False
-    
-    window.fill(back_color)
-    reset(player_l)
-    reset(player_r)
-    reset(ball)
+    if not finish:
+        
+        window.fill(back_color)
+        reset(player_l)
+        reset(player_r)
+        reset(ball)
 
-    player_l.update_l()
-    player_r.update_r()
+        player_l.update_l()
+        player_r.update_r()
+
+        ball.rect.x += ball_speed_x
+        ball.rect.y += ball_speed_y
+
+        if ball.rect.y < 0 or ball.rect.y >= 450:
+            ball_speed_y *= -1
+
+        if sprite.collide_rect(ball, player_l) or sprite.collide_rect(ball, player_r):
+            ball_speed_x *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(player_l_lose, [350, 220])
+
+        if ball.rect.x >= 800:
+            finish = True
+            window.blit(player_r_lose, [350, 220])
+
+    for ev in event.get():
+            if ev.type == QUIT:
+                running = False
 
 
     clock.tick(60)
